b381b9e4a14b1f368b6a97c3d89065cd86f5e007
Paul Anderson <pauleanderson@gmail.com>
2020-04-01 12:33:31 -0700

    update

---
+++ tests/test_Syllabus.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append(".")
+
+# Import the student solutions
+import Syllabus
+
+import pathlib
+DIR=pathlib.Path(__file__).parent.absolute()
+
+import joblib
+answers = joblib.load(str(DIR)+"/answers_Syllabus.joblib")
+
+def test_question_1():
+    assert Syllabus.answer_question_1() == answers['answer_question_1']
+    
+def test_question_2():
+    assert Syllabus.answer_question_2() == answers['answer_question_2']
+    
+def test_question_3():
+    assert Syllabus.answer_question_3() == answers['answer_question_3']
+    
+def test_question_4():
+    assert Syllabus.answer_question_4() == answers['answer_question_4']
+    
+def test_question_5():
+    assert Syllabus.answer_question_5() == answers['answer_question_5']
