c67bc1d530c465f3b0cb8d5390b1252dd5c74067
Paul Anderson <pauleanderson@gmail.com>
2020-04-01 12:01:58 -0700

    update

--- tests/test_Syllabus.py
+++ tests/test_Syllabus.py
@@ -8,10 +8,10 @@ import pathlib
 DIR=pathlib.Path(__file__).parent.absolute()
 
 import joblib
-joblib.load(DIR+"/answers_Syllabus.joblib")
+answers = joblib.load(str(DIR)+"/answers_Syllabus.joblib")
 
 def test_question_1():
-    assert answer_question_1() == answers['answer_question_1']
+    assert Syllabus.answer_question_1() == answers['answer_question_1']
     
 def test_question_2():
-    assert answer_question_2() == answers['answer_question_2']
+    assert Syllabus.answer_question_2() == answers['answer_question_2']
