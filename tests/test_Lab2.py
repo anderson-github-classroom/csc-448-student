dac49b3560ef8cbd4d24f0245feffeec010e8303
Paul Anderson <pauleanderson@gmail.com>
2020-04-14 00:57:31 -0700

    update

--- tests/test_Lab2.py
+++ tests/test_Lab2.py
@@ -36,3 +36,7 @@ def test_exercise_3b():
     
 def test_exercise_3c():
     assert np.all(nx.adjacency_matrix(Lab2.additive_phylogeny(Lab2.Dorig,len(Lab2.Dorig)+1)).todense() == answers['answer_exercise_3c'])
+    
+# git clone https://github.com/anderson-github-classroom/csc-448-student ../csc-448-student && sudo -H pip3 install -r ../csc-448-student/requirements.txt
+
+# pytest ../csc-448-student/tests/test_Lab1.py::test_exercise_1
