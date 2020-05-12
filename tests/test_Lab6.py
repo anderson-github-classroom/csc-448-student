0219bce6fc12d9312c5e7912939b42e43a6926ae
Paul Anderson <pauleanderson@gmail.com>
2020-05-12 13:55:15 -0700

    update

--- tests/test_Lab6.py
+++ tests/test_Lab6.py
@@ -16,7 +16,7 @@ answer_tol = 1e-10
 
 def test_exercise_1():
     answer = Lab6.trie_construction(Lab6.patterns2)
-    assert np.all(Lab6.to_adj(answer) == answers['answer_exercise_1'])
+    assert np.all(Lab6.to_adj(answer).values == answers['answer_exercise_1'].values)
     
 def test_exercise_2():
     answer = Lab6.trie_matching("bananablahblahantennanabnablkjdf",Lab6.trie2)
@@ -24,12 +24,12 @@ def test_exercise_2():
     
 def test_exercise_3():
     answer = Lab6.suffix_trie("panamabananas$")
-    assert np.all(Lab6.to_adj(answer) == answers['answer_exercise_3'])
+    assert np.all(Lab6.to_adj(answer).values == answers['answer_exercise_3'].values)
     
 def test_exercise_4():
     answer,discard = Lab6.modified_suffix_trie("panamabananas$")
-    assert np.all(Lab6.to_adj(answer) == answers['answer_exercise_4'])
+    assert np.all(Lab6.to_adj(answer).values == answers['answer_exercise_4'].values)
     
 def test_exercise_5():
     answer = Lab6.suffix_tree_construction("panamabananas$")
-    assert np.all(Lab6.to_adj(answer) == answers['answer_exercise_5'])
+    assert np.all(Lab6.to_adj(answer).values == answers['answer_exercise_5'].values)
