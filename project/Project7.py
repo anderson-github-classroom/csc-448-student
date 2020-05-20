565368d570f5111c403923e254bdeee3287f6750
Paul Anderson <pauleanderson@gmail.com>
2020-05-20 15:17:19 -0700

    update

---
+++ project/Project7.py
@@ -0,0 +1,24 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,md,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# ## Project Part 7(a.k.a. Project 7 on Schedule)
+# For project 7, we are into the final stretch with many things to do. I have put many comments in the document that should give you direction on what should be added/modified/etc. But you can of course use your own discretion as well. The general feedback from our exercise on Monday was that we should definitely narrow to the two questions. There were a few other suggestions that I've incorporated in my inline comments. I feel like we can pull this off, and I'm happy and proud to be part of this group effort. I hope you can feel the same as you continue to edit and add to our document.
+
+# ### Link to clone the repository
+# Here is a link to the project repository.
+#
+# https://github.com/anderson-github-classroom/csc-448-project
+#
+# The website can be viewed at https://anderson-github-classroom.github.io/csc-448-project/.
