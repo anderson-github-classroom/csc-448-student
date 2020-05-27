af33fbb58a57f937b34d3db481359d70623e27ff
Paul Anderson <pauleanderson@gmail.com>
2020-05-27 15:25:39 -0700

    update

--- tests/test_Lab8.py
+++ tests/test_Lab8.py
@@ -17,7 +17,7 @@ answer_tol = 1e-10
 def test_exercise_1():
     num_blocks = len(Lab8.lens)
     assert num_blocks == answers['exercise_1_num_blocks']
-    lower = np.all(Lab8.counts > answers['exercise_1_means'] - 1*answers['exercise_1_stdevs'])
+    lower = np.all(Lab8.counts > answers['exercise_1_means'] - 3*answers['exercise_1_stdevs'])
     assert lower
-    upper = np.all(Lab8.counts < answers['exercise_1_means'] + 1*answers['exercise_1_stdevs'])
+    upper = np.all(Lab8.counts < answers['exercise_1_means'] + 3*answers['exercise_1_stdevs'])
     assert upper
