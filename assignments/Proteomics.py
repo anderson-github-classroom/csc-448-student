d99b022eadfaf0e7fd85fe567e1623ca1b003069
Paul Anderson <pauleanderson@gmail.com>
2020-10-05 19:43:08 +0000

    update

---
+++ assignments/Proteomics.py
@@ -0,0 +1,35 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,md,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# + [markdown] slideshow={"slide_type": "slide"} hideCode=false hidePrompt=false
+# # Name(s)
+# Your name here
+
+# + [markdown] slideshow={"slide_type": "subslide"} hideCode=false hidePrompt=false
+# **Instructions:** This is an individual assignment. Complete the following code and push to officially get your score.
+
+# + [markdown] slideshow={"slide_type": "subslide"} hideCode=false hidePrompt=false
+# I am providing the autograder answers locally so you may test your code before pushing. I will be reviewing your submissions, and if I find you are circumventing the autograder in any manner, you will receive a 0 on this assignment and your case will be reported to the honor board for review. i.e., approach the assignment in a genuine manner and you have nothing to worry about.
+
+# + [markdown] slideshow={"slide_type": "slide"} hideCode=false hidePrompt=false
+# ### Answer the following
+# What are the interesting computational problems/questions that were mentioned in the talk?
+
+# + hideCode=false slideshow={"slide_type": "subslide"} hidePrompt=false
+answer="""
+
+PUT HERE ANSWER HERE
+
+"""
---
+++ tests/test_Proteomics.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append(".")
+
+# Import the student solutions
+import Proteomics 
+
+import pathlib
+DIR=pathlib.Path(__file__).parent.absolute()
+
+def test_question_1():
+    assert len(answer.strip()) >= 150 
+    
