15a06f315bc1e5a3aacc538dce2fa8e45f724b4b
Paul Anderson <pauleanderson@gmail.com>
2020-10-16 21:55:57 +0000

    update

--- tests/test_Lab5.py
+++ tests/test_Lab5.py
@@ -2,21 +2,21 @@ import sys
 sys.path.append(".")
 
 # Import the student solutions
-import Lab2
+import Lab5
 
 import pathlib
 DIR=pathlib.Path(__file__).parent.absolute()
 
 import joblib
-answers = joblib.load(str(DIR)+"/answers_Lab2.joblib")
+answers = joblib.load(str(DIR)+"/answers_Lab5.joblib")
 print("Keys",answers.keys())
 
 import networkx as nx
 import numpy as np
 import copy
 
-D = copy.copy(Lab2.Dorig)
-limbLength = Lab2.limb(D,D.index[-1]) # our algorithm will choose the last node
+D = copy.copy(Lab5.Dorig)
+limbLength = Lab5.limb(D,D.index[-1]) # our algorithm will choose the last node
 n = D.index[-1]
 Dtrimmed = D.drop(n).drop(n,axis=1)
 for j in Dtrimmed.index:
@@ -24,22 +24,22 @@ for j in Dtrimmed.index:
     D.loc[n,j] = D.loc[j,n]
 
 def test_exercise_1():
-    assert (Lab2.compute_d(Lab2.G) == answers['answer_exercise_1']).all().all()
+    assert (Lab5.compute_d(Lab5.G) == answers['answer_exercise_1']).all().all()
 
 def test_exercise_2():
-    assert Lab2.limb(Lab2.Dorig,"v4") == answers['answer_exercise_2']
+    assert Lab5.limb(Lab5.Dorig,"v4") == answers['answer_exercise_2']
 
 def test_exercise_3a():
-    assert Lab2.find(D,"v4") == answers['answer_exercise_3a']
+    assert Lab5.find(D,"v4") == answers['answer_exercise_3a']
     
 def test_exercise_3b():
-    assert np.all(nx.adjacency_matrix(Lab2.base_case(Lab2.Dorig.iloc[:2,:].iloc[:,:2])).todense() == answers['answer_exercise_3b'])
+    assert np.all(nx.adjacency_matrix(Lab5.base_case(Lab5.Dorig.iloc[:2,:].iloc[:,:2])).todense() == answers['answer_exercise_3b'])
     
 def test_exercise_3c():
-    assert np.all(nx.adjacency_matrix(Lab2.additive_phylogeny(Lab2.Dorig,len(Lab2.Dorig)+1)).todense() == answers['answer_exercise_3c'])
+    assert np.all(nx.adjacency_matrix(Lab5.additive_phylogeny(Lab5.Dorig,len(Lab5.Dorig)+1)).todense() == answers['answer_exercise_3c'])
 
 def test_exercise_4():
-    assert np.all(nx.adjacency_matrix(Lab2.additive_phylogeny(Lab2.D_sars,len(Lab2.D_sars)+1)).todense() == answers['answer_exercise_4'])
+    assert np.all(nx.adjacency_matrix(Lab5.additive_phylogeny(Lab5.D_sars,len(Lab5.D_sars)+1)).todense() == answers['answer_exercise_4'])
     
 # git clone https://github.com/anderson-github-classroom/csc-448-student ../csc-448-student && sudo -H pip3 install -r ../csc-448-student/requirements.txt
 
